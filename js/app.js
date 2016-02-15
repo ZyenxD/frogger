b3d0c58f82e37347343517f44ed544a6daf98b20
ZyenxD <neyc30@gmail.com>
2016-02-14 21:40:45 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = 10;
+    this.y = 30;
+    this.width = 70;
+    this.height = 90;
+    this.speed = Math.random()*2;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -10,10 +15,25 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+Enemy.prototype.reset = function(){
+    this.x = 0;
+    this.speed = Math.random() *4;
+    this.y = Math.floor(Math.random()*(300-50));
+    console.log("reset enemys");
+};
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += (100*dt*this.speed);
+    if(this.x>500){
+        this.reset();
+    }
+    console.log("enemy x= "+this.x,"enemy y="+this.y);
+    if(this.x === player.x && this.y === player.y){
+        player.reset();
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +44,64 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+player = function(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 100;
+    this.y = 400;
+}
+//player.prototype = Object.create(Enemy.prototype);
+player.prototype.reset = function(){
+    this.x = 100;
+    this.y = 400;
+}
+player.prototype.update = function(dt){
+    if(player.y===0){
+        player.reset();
+    }
+    console.log('player x = ', this.x, 'player y = ', this.y);
+}
+player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+}
+player.prototype.handleInput = function(direction){
+    switch(direction){
+        case'up':
+        this.y -= 50;
+        break;
+        case'down':
+        if(this.y===400){
+            //don't do nothig
+        }else{
+            this.y += 50;
+        }
+        break;
+        case'left':
+        if(this.x===0){
+            //nothing either
+        }else{
+            this.x -= 50;
+        }
+        break;
+        case'right':
+        if(this.x===400){
+            //DON'T MOVE >:|
+        }else{
+            this.x += 50;
+        }
+        break;
+    }
 
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var allEnemies = [];
+for(i = 1;i<=3;i++){
+ allEnemies[i]= new Enemy();
+}
+var player = new player();
 
 
 
